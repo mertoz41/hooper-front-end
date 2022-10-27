477f43faeae945e16b4601224c8b0c281a0459a6
mert <mertozka41@gmail.com>
2022-10-27 12:33:23 -0400

    right server

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -43,6 +43,7 @@ const getTiming = (created_at) => {
 };
 
 const API_ROOT = "https://polar-plains-09713.herokuapp.com";
+
 const errorToast = {
   title: "An error occurred.",
   description: "Please try again later.",
